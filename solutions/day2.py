58769c4b9cae51950f240d18a3801ceec1d0880f
Leonardo Cornelatti <lcornelatti@itbit.com>
2022-12-03 16:09:07 +0000

    fix

--- solutions/day2.py
+++ solutions/day2.py
@@ -1,92 +1,26 @@
-
-file1 = open('./inputs/day2.txt', 'r')
-lines = file1.readlines()
-
-
-def part1():
-    total = 0
-    for count, line in enumerate(lines):
-
-        l = line.strip().split()
-
-        opponent = l[0]
-        mymove = l[1]
-
-        outcomePoints = 0
-        movepoints = 0
-        if opponent == "A":
-            if mymove == "X":
-                outcomePoints = 3
-            elif mymove == "Y":
-                outcomePoints = 6
-            elif mymove == "Z":
-                outcomePoints = 0
-
-        elif opponent == "B":
-            if mymove == "X":
-                outcomePoints = 0
-            elif mymove == "Y":
-                outcomePoints = 3
-            elif mymove == "Z":
-                outcomePoints = 6
-
-        elif opponent == "C":
-            if mymove == "X":
-                outcomePoints = 6
-            elif mymove == "Y":
-                outcomePoints = 0
-            elif mymove == "Z":
-                outcomePoints = 3
-
-        if mymove == "X":
-            movepoints = 1
-        elif mymove == "Y":
-            movepoints = 2
-        elif mymove == "Z":
-            movepoints = 3
-        total += movepoints + outcomePoints
-
-    print(total)
+lines = [line.strip().replace("X", "A").replace(
+    "Y", "B").replace("Z", "C") for line in open('./inputs/day2.txt', 'r').readlines()]
+letterToPoints = {"A": 1, "B": 2, "C": 3}
 
 
-def part2():
-    total = 0
-    for count, line in enumerate(lines):
-
-        l = line.strip().split()
-
-        opponent = l[0]
-        mymove = l[1]
-
-        movepoints = 0
-        if opponent == "A":
-            if mymove == "X":
-                movepoints += 3
-            elif mymove == "Y":
-                movepoints += 1 + 3
-            elif mymove == "Z":
-                movepoints += 2 + 6
-
-        elif opponent == "B":
-            if mymove == "X":
-                movepoints += 1
-            elif mymove == "Y":
-                movepoints += 2 + 3
-            elif mymove == "Z":
-                movepoints += 3 + 6
-
-        elif opponent == "C":
-            if mymove == "X":
-                movepoints += 2
-            elif mymove == "Y":
-                movepoints += 3 + 3
-            elif mymove == "Z":
-                movepoints += 1 + 6
-
-        total += movepoints
-
-    print(total)
+def lineToPointsPart1(line):
+    if line in ["A C", "B A", "C B"]:
+        return 0
+    elif line in ["A B", "B C", "C A"]:
+        return 6
+    else:
+        return 3
 
 
-part1()
-part2()
+def lineToPointsPart2(line):
+    if line[2] == "A":
+        return (letterToPoints[line[0]] - 1) or 3
+    if line[2] == "B":
+        return 3 + letterToPoints[line[0]]
+    if line[2] == "C":
+        return 6 + ((letterToPoints[line[0]] + 1) % 3 or 3)
+
+
+print(sum([lineToPointsPart1(line) + letterToPoints[line[2]]
+      for line in lines]))
+print(sum([lineToPointsPart2(line) for line in lines]))
