2e5566b4ccec964352b2518b3b58abe05a8ba0cf
Leonardo Cornelatti <lcornelatti@itbit.com>
2022-12-05 19:52:16 +0000

    optimize

--- solutions/day1.py
+++ solutions/day1.py
@@ -1,20 +1,8 @@
 from collections import defaultdict
+from itertools import groupby, zip_longest
 from heapq import heappush
 
-lines = [line.strip() for line in open('./inputs/day1.txt', 'r').readlines()]
-print(cols)
-elfNum = 0
-caloriesPerElf = defaultdict(int)
-topElves = []
-
-for line in lines:
-    if line == "":
-        elfNum += 1
-        continue
-    caloriesPerElf[elfNum] += int(line)
-
-for calories in caloriesPerElf.values():
-    heappush(topElves, -calories)
-
-print(-topElves[0])
-print(-topElves[0]-topElves[1]-topElves[2])
+cpe = [sum(map(int, map(lambda x: x.strip(), group)))
+       for k, group in groupby(open('./inputs/day1.txt', 'r').readlines(), lambda x: x == "\n") if not k]
+print(max(cpe))
+print(sum([cpe.pop(cpe.index(max(cpe))) for i in range(3)]))
